98db2f16e8705d20fc2d95a8d3649515a96620eb
joaco lema <joaco.lema@hotmail.com>
2021-12-29 13:44:35 -0300

    agrgeamos validation de usuario creado

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,18 +3,32 @@ const Usuario = require('../models/Usuario');
 
 const crearUsuario = async (req, res = express.response) => {
 
-    // const {name, email, password} = req.body;
+    const { name, email, password } = req.body;
 
     try {
 
-        const usuario = new Usuario(req.body);
+        let usuario = await Usuario.findOne({ email });
+
+        if (usuario) {
+            return res.status(400).json(
+                {
+                    "ok": false,
+                    "msg": "Usuario con ese mail ya existe"
+                }
+            );
+
+        }
+
+        usuario = new Usuario(req.body);
 
         await usuario.save();
 
         res.status(201).json(
             {
                 "ok": true,
-                "msg": "Usuario creado exitosamente"
+                "msg": "Usuario creado exitosamente",
+                "uid": usuario._id,
+                "name": usuario.name
             }
         );
 
